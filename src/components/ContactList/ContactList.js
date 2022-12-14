dd3d055ffa3d7de44e4f9604b4f4e19e2411e0f7
Misha Levko <lvk.misha@gmail.com>
2022-12-14 16:12:53 +0200

    fixed useEffect

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -2,20 +2,11 @@ import { ListContact, ContactListList } from './ContactList.styled';
 import { useDispatch, useSelector } from 'react-redux';
 import * as phonebookOperations from 'components/redux/phonebookOperations';
 
-import { useEffect } from 'react';
 import { selectContacts } from 'components/redux/selectors';
 
 export default function ContactList() {
   const filterValue = useSelector(state => state.filter);
   const dispatch = useDispatch();
-
-  useEffect(() => {
-    const controller = new AbortController();
-    dispatch(phonebookOperations.fetchContacts(controller.signal));
-    return () => {
-      controller.abort();
-    };
-  }, [dispatch]);
 
   const contactsRedux = useSelector(selectContacts);
   const filterContacts = () => {
--- src/components/Phonebook/Phonebook.js
+++ src/components/Phonebook/Phonebook.js
@@ -2,11 +2,23 @@ import ContactForm from 'components/ContactForm/ContactForm';
 import Filter from 'components/Filter/Filter';
 import ContactList from 'components/ContactList/ContactList';
 import { Container } from './PhoneBook.styled';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { selectContacts } from 'components/redux/selectors';
+import * as phonebookOperations from 'components/redux/phonebookOperations';
+import { useEffect } from 'react';
 
 export default function Phonebook() {
   const contactsRedux = useSelector(selectContacts);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const controller = new AbortController();
+    dispatch(phonebookOperations.fetchContacts(controller.signal));
+    return () => {
+      controller.abort();
+    };
+  }, [dispatch]);
 
   return (
     <Container className="Phonebook_container">
